928907a217efba5a789ad89ad82cf2d1b89aa985
Alex <alex.chaves@redes.unb.br>
2016-05-24 16:46:02 -0300

    > updated tests

--- UIoT/tests/validators/RequestValidatorTest.php
+++ UIoT/tests/validators/RequestValidatorTest.php
@@ -1,15 +1,42 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: uiot
- * Date: 24/05/16
- * Time: 14:00
- */
+namespace UIoT\tests\validators;
+
+use PHPUnit_Framework_TestCase;
+use UIoT\validators\RequestValidator;
+
 class RequestValidatorTest extends PHPUnit_Framework_TestCase
 {
-    public function test()
-    {
+    private $wrongFormatRequests;
+    private $happyRequest; //TODO
+    private $requestValidator;
+    private $address; //TODO
 
+    public function localSetUp()
+    {
+        $this->wrongFormatRequests = Array(
+            null,
+            true,
+            false,
+            0,
+            1,
+            -1,
+            new \stdClass(),
+            Array(),
+            "");
+        $this->address = "";//TODO
+        $this->happyRequest = "http://".address;
+        $this->requestValidator = new RequestValidator();
     }
+    /**
+     * @requires localSetUp();
+     */
+    public function testValidate()
+    {
+        foreach($this->wrongFormatRequests as $request )
+        {
+            assert($this->requestValidator->validate($request) );  
+        };
+    }
+
 }
--- UIoT/validators/RequestValidator.php
+++ UIoT/validators/RequestValidator.php
@@ -2,7 +2,6 @@
 
 namespace UIoT\validators;
 
-
 use UIoT\model\UIoTRequest;
 
 class RequestValidator
@@ -11,54 +10,73 @@ class RequestValidator
      * @param UIoTRequest $request
      * @return boolean
      */
-    public function validateFormat(UIoTRequest $request)
+    public function validate(UIoTRequest $request){
+        $isValid =
+            $this->validateFormat($request) &&
+            $this->validateProtocol($request) &&
+            $this->validateMethod($request) &&
+            $this->validateResource($request) &&
+            $this->validateParameters($request) &&
+            $this->validateValues($request);
+        
+        if($isValid) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    /**
+     * @param UIoTRequest $request
+     * @return boolean
+     */
+    private function validateFormat(UIoTRequest $request)
     {
-
+        return false;
     }
 
     /**
      * @param UIoTRequest $request
      * @return boolean
      */
-    public function validateResource(UIoTRequest $request)
+    private function validateResource(UIoTRequest $request)
     {
-
+        return false;
     }
 
     /**
      * @param UIoTRequest $request
      * @return boolean
      */
-    public function validateParameters(UIoTRequest $request)
+    private function validateParameters(UIoTRequest $request)
     {
-
+        return false;
     }
 
     /**
      * @param UIoTRequest $request
      * @return boolean
      */
-    public function validateValues(UIoTRequest $request)
+    private function validateValues(UIoTRequest $request)
     {
-
+        return false;
     }
 
     /**
      * @param UIoTRequest $request
      * @return boolean
      */
-    public function validateMethod(UIoTRequest $request)
+    private function validateMethod(UIoTRequest $request)
     {
-
+        return false;
     }
 
     /**
      * @param UIoTRequest $request
      * @return boolean
      */
-    public function validateProtocol(UIoTRequest $request)
+    private function validateProtocol(UIoTRequest $request)
     {
-
+        return false;
     }
-
 }
