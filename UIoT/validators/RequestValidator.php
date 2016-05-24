92ca955414679705c095099a3ddd621d391b0180
Claudio Santoro <claudio.santoro@redes.unb.br>
2016-05-24 13:59:03 -0300

    Revert "added test folder"
    This reverts commit 7d7424d8e86aea48cf8d8c1f33a4772c867c0c65.

---
+++ UIoT/validators/RequestValidator.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace UIoT\validators;
+
+
+use UIoT\model\UIoTRequest;
+
+class RequestValidator
+{
+    /**
+     * @param UIoTRequest $request
+     * @return boolean
+     */
+    public function validateFormat(UIoTRequest $request)
+    {
+
+    }
+
+    /**
+     * @param UIoTRequest $request
+     * @return boolean
+     */
+    public function validateResource(UIoTRequest $request)
+    {
+
+    }
+
+    /**
+     * @param UIoTRequest $request
+     * @return boolean
+     */
+    public function validateParameters(UIoTRequest $request)
+    {
+
+    }
+
+    /**
+     * @param UIoTRequest $request
+     * @return boolean
+     */
+    public function validateValues(UIoTRequest $request)
+    {
+
+    }
+
+    /**
+     * @param UIoTRequest $request
+     * @return boolean
+     */
+    public function validateMethod(UIoTRequest $request)
+    {
+
+    }
+
+    /**
+     * @param UIoTRequest $request
+     * @return boolean
+     */
+    public function validateProtocol(UIoTRequest $request)
+    {
+
+    }
+
+}
---
+++ UIoT/view/RequestInput.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace UIoT\view;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use UIoT\control\RequestController;
+use UIoT\model\UIoTRequest;
+use UIoT\model\UIoTResponse;
+use UIoT\util\ExceptionHandler;
+use UIoT\util\RequestRouter;
+
+/**
+ * Class RequestInput
+ *
+ * @package UIoT\view
+ *
+ * @property RequestController $requestControl
+ * @property RequestRouter $requestRouter
+ * @property UIoTRequest $requestData
+ * @property Response $responseData
+ */
+class RequestInput
+{
+    /**
+     * @var RequestController
+     */
+    private $requestControl;
+
+    /**
+     * @var RequestRouter
+     */
+    private $requestRouter;
+
+    /**
+     * @var UIoTRequest
+     */
+    private $requestData;
+
+    /**
+     * @var UIoTResponse
+     */
+    private $responseData;
+
+    /**
+     * RequestInput constructor.
+     */
+    public function __construct()
+    {
+        self::registerExceptionHandler();
+
+        self::setRequestControl(new RequestController());
+
+        self::setRequestRouter(new RequestRouter());
+
+        self::setRequestData(UIoTRequest::createFromGlobals());
+
+        self::getRequestData()->setRequestValidation();
+
+        self::getRequestData()->assignRequestData();
+
+        self::setResponseData();
+    }
+
+    /**
+     * Register Exception Handler
+     */
+    private function registerExceptionHandler()
+    {
+        set_exception_handler(array(ExceptionHandler::getInstance(), 'handleException'));
+    }
+
+    /**
+     * Sets the request controller attribute | @see $requestControl
+     *
+     * @param RequestController $requestControl
+     */
+    private function setRequestControl(RequestController $requestControl)
+    {
+        $this->requestControl = $requestControl;
+    }
+
+    /**
+     * Sets the request router attribute | @see $requestRouter
+     *
+     * @param RequestRouter $requestRouter
+     */
+    private function setRequestRouter(RequestRouter $requestRouter)
+    {
+        $this->requestRouter = $requestRouter;
+    }
+
+    /**
+     * Set Current Request Data
+     *
+     * @param Request $requestData
+     */
+    private function setRequestData(Request $requestData)
+    {
+        $this->requestData = $requestData;
+    }
+
+    /**
+     * Set Response based on his Request
+     *
+     * @param Request $requestData
+     */
+    private function setResponseData(Request $requestData = NULL)
+    {
+        $this->responseData = new UIoTResponse();
+
+        $this->responseData->prepare($requestData == NULL ? $this->requestData : $requestData);
+    }
+
+    /**
+     * Get Request Data
+     *
+     * @return UIoTRequest
+     */
+    public function getRequestData()
+    {
+        return $this->requestData;
+    }
+
+    /**
+     * Get Response Data
+     *
+     * @return UIoTResponse
+     */
+    public function getResponseData()
+    {
+        return $this->responseData;
+    }
+
+    /**
+     * Starts the Request creation and submission process
+     *
+     * @return array|bool|string
+     */
+    public function route()
+    {
+        return $this->requestRouter->submitRequest($this->requestControl->createRequest($this));
+    }
+
+    /**
+     * @return RequestRouter
+     */
+    public function getRequestRouter()
+    {
+        return $this->requestRouter;
+    }
+}
