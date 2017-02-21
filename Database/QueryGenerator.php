3e7ea5efe722fe74215f9fe9e31d5f0349c655ff
Lucas Meine <RA21604398@uniceub.edu.br>
2017-02-21 19:34:06 -0300

    Devmind commit in 2017-02-21 19:34:10

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -90,7 +90,7 @@ Class QueryGenerator
           }
           $request->getBody()['services'] =  $services;
           
-          var_dump($request->getBody());exit;
+          var_dump($services);exit;
           
           $request->treatedBody = json_encode(array_merge($request->getBody(),$requestBody));
           $request->token = $requestBody['tokenId'];
