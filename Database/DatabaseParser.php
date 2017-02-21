0d90baefb47d675acd995d396ceedbf875491d19
Lucas Meine <RA21604398@uniceub.edu.br>
2017-02-21 20:25:52 -0300

    Devmind commit in 2017-02-21 20:25:56

--- Database/DatabaseParser.php
+++ Database/DatabaseParser.php
@@ -57,7 +57,8 @@ class DatabaseParser
                 if($responseRows->request->service == true)
                 {
                     $response->services = array();
-                  
+                      
+                      var_dump(json_decode($responseRows->request->treatedBody)->services);exit;
                     foreach(json_decode($responseRows->request->treatedBody)->services as $key=>$service)
                     {
                         $response->services[] = array('service_id' => $key, 'service_name' => $service->service_name);
