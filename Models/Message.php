0f39d00d1d205f16a84bf101eeeaea30c3c51967
Lucas Meine <RA21604398@uniceub.edu.br>
2017-02-21 17:43:41 -0300

    Devmind commit in 2017-02-21 17:43:45

--- Models/Message.php
+++ Models/Message.php
@@ -112,8 +112,8 @@ class Message
 
   public function message_out()
   {
-      exit($this->get_code_http());
-    if($this->get_code_http() === 0)
+      
+    if($this->get_code_http() === (int) 0)
     {
         exit('BEM LOCO');
         $this->set_code_http(200);
