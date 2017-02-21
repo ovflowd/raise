3c6e1f984360cfc688c2deccd442553c7ba433a2
Lucas Meine <RA21604398@uniceub.edu.br>
2017-02-21 18:32:34 -0300

    Devmind commit in 2017-02-21 18:32:38

--- Database/DatabaseParser.php
+++ Database/DatabaseParser.php
@@ -52,7 +52,7 @@ class DatabaseParser
             
             $array = (array)$this->getBucket();
             
-             var_dump($array);exit;
+             var_dump($array[1]);exit;
             
             $response = (new MessageOutPut())->messageHttp(200);
             if($responseRows->bucket === "client")
