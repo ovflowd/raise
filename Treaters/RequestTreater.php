97ada11fdb7040fc3be2bb4a10de0c76963d4a96
Lucas Meine <RA21604398@uniceub.edu.br>
2017-02-21 17:35:13 -0300

    Devmind commit in 2017-02-21 17:35:17

--- Treaters/RequestTreater.php
+++ Treaters/RequestTreater.php
@@ -122,7 +122,7 @@ class RequestTreater
     {
         if($request->getPath()['bucket'] === null && empty($request->getPath()['address']) && $request->getPath()['method'] == null)
         {
-            $request->setResponseCode(32132);
+            $request->setResponseCode(403);
             $request->setValid(false);
             return false;
         }
