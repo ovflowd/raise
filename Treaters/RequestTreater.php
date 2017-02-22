81e93a2bfb2c29a41f5b5cb0b202a963600bb6e3
Lucas Meine <RA21604398@uniceub.edu.br>
2017-02-22 17:12:41 -0300

    Devmind commit in 2017-02-22 17:12:46

--- Treaters/RequestTreater.php
+++ Treaters/RequestTreater.php
@@ -189,6 +189,7 @@ class RequestTreater
             $query->namedParams(array('token' => $token));
             $parameters = $database->query($query)->rows;
             $compare = $parameters[$service['service_id']];
+            var_dump($compare);exit;
             $compare = json_decode(json_encode($compare), true);
             $compare = $compare['services'][$service['service_id']]['parameters'];
             var_dump($compare);exit;
