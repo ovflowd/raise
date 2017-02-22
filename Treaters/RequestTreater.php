3311e590cb7a5ad67b45176075ec77e057374e2f
Lucas Meine <RA21604398@uniceub.edu.br>
2017-02-22 15:17:23 -0300

    Devmind commit in 2017-02-22 15:17:27

--- Treaters/RequestTreater.php
+++ Treaters/RequestTreater.php
@@ -178,11 +178,10 @@ class RequestTreater
     {
          $token = $request->getBody()['token'];
          $services = $request->getBody()[0];
-            
+            var_dump($request->getBody());exit;
          foreach($services as $service)
          {
-             var_dump($service['service_id']);
-             var_dump($service['values']);
+             var_dump($service);
          }
     }
 
