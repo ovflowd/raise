8c0908391c36c650023f520172ffc1275cf567fe
Lucas Meine <RA21604398@uniceub.edu.br>
2017-02-22 16:25:11 -0300

    Devmind commit in 2017-02-22 16:25:15

--- Treaters/RequestTreater.php
+++ Treaters/RequestTreater.php
@@ -190,7 +190,6 @@ class RequestTreater
             $parameters = $database->query($query)->rows;
             $compare = $parameters[$service['service_id']-1];
             $compare = json_decode(json_encode($compare), true);
-             echo "<br><br>";
              var_dump($compare);
              
              
