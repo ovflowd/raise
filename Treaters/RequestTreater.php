7302549373af28f0b7ceeb51fd0a50b6235b9e18
Lucas Meine <RA21604398@uniceub.edu.br>
2017-02-22 17:21:51 -0300

    Devmind commit in 2017-02-22 17:21:56

--- Treaters/RequestTreater.php
+++ Treaters/RequestTreater.php
@@ -188,9 +188,11 @@ class RequestTreater
             $query = \CouchbaseN1qlQuery::fromString('SELECT services FROM client WHERE `tokenId` = $token');
             $query->namedParams(array('token' => $token));
             $parameters = $database->query($query)->rows;
-            $compare = json_decode(json_encode($compare), true);
-            var_dump($compare);exit;
             $compare = $parameters[$service['service_id']];
+            var_dump($compare);exit;
+            $compare = json_decode(json_encode($compare), true);
+            
+            
            
             
             $compare = $compare['services'][$service['service_id']]['parameters'];
