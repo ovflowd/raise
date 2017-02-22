26d82f15a7b574db0136ccdbd9f5bced02c5d8a0
Lucas Meine <RA21604398@uniceub.edu.br>
2017-02-22 18:15:30 -0300

    Devmind commit in 2017-02-22 18:15:34

--- Treaters/RequestTreater.php
+++ Treaters/RequestTreater.php
@@ -179,6 +179,13 @@ class RequestTreater
          $token = $request->getBody()['token'];
          $services = $request->getBody()[0];
          
+             $database = (new DatabaseParser($request))->getBucket();
+            $query = \CouchbaseN1qlQuery::fromString('SELECT * FROM token WHERE `tokenId` = $token');
+            $query->namedParams(array('token' => $token));
+            $parameters = $database->query($query)->rows;
+            
+            var_dump($parameters);exit;
+         
          foreach($services as $service)
          {
              $service['service_id'];
