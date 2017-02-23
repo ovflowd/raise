2a4dfc195efeaf383f1dc5bc5727bbf1237ceea0
Iury Montenegro <iury2807@hotmail.com>
2017-02-23 09:59:54 -0300

    Devmind commit in 2017-02-23 09:59:59

--- Tests/RequestTest.php
+++ Tests/RequestTest.php
@@ -40,7 +40,7 @@ class RequestTester
 		$url = "http://{$this->raise_ip}/service/register";
 
 		$body = json_encode(array(
-					"services" => array(array('name'=>'temperature','parameters'=>array('temp'=>'vagabond') , 'return_type' => 'float'),array('name'=>'pressure','parameters'=>array('press'=>'double') , 'return_type' => 'float')),
+					"services" => array(array('name'=>'temperature','parameters'=>array('temp'=>'vagabond') , 'return_type' => 'float'),array('name'=>'pressure','parameters'=>array('batata'=>'double') , 'return_type' => 'float')),
 						"timestamp" => round(microtime(true) *1000),
 						'tokenId' => $token
 						));
