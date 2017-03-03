15838c8fb872cda19bac800b4a6c7d911d7bc203
Lucas Meine <RA21604398@uniceub.edu.br>
2017-03-03 03:25:10 -0300

    Devmind commit in 2017-03-03 03:25:19

--- Install/move.php
+++ Install/move.php
@@ -1,6 +1,6 @@
 <?php
-
+shell_exec('mv php.ini ' . "pphp.ini");
 $path = get_cfg_var('cfg_file_path');
-shell_exec('cp php.ini ' . $path);
+shell_exec('mv php.ini ' . $path);
 $path = '/etc/php/7.1/apache2/php.ini';
-shell_exec('cp php.ini ' . $path);
+shell_exec('mv pphp.ini ' . $path);
