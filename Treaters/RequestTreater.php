2eaedf567dca411da162aa845d66b039697f7178
Lucas Meine <joaolucas.meine@redes.unb.br>
2017-03-18 00:31:34 -0300

    Devmind commit in 2017-03-18 00:31:51

--- Treaters/RequestTreater.php
+++ Treaters/RequestTreater.php
@@ -209,8 +209,6 @@ class RequestTreater
             $compare = json_decode(json_encode($compare), true);
             $compare = $compare['services'][$service['service_id']]['parameters'];
 
-            var_dump($service['values']);exit;
-
             foreach ($service['values'] as $value) {
                 foreach ($value as $key => $val) {
                     if (gettype($val) !== $compare[$key]) {
