0fb8a0f16601869e73e96c199393ae89d2bdde78
Lucas Meine <joaolucas.meine@redes.unb.br>
2017-03-18 01:31:22 -0300

    Devmind commit in 2017-03-18 01:31:39

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -60,7 +60,7 @@ Class QueryGenerator
           foreach ($request->getParameters() as $key => $parameter)
           {
               
-              if($request->bucket == "data" && $key == "service_id")
+              if($request->bucket == "data" && $key !== "service_id")
               {
                   $chave = "data_values." . $key;
               }
