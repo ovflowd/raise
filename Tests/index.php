6dd1fc539aaac71b8d723ff42212bf6a26d2f18f
Lucas Meine <joaolucas.meine@redes.unb.br>
2017-03-17 23:14:45 -0300

    Devmind commit in 2017-03-17 23:15:02

--- Tests/index.php
+++ Tests/index.php
@@ -3,11 +3,9 @@
 ini_set('display_errors', 1);
 include 'RequestTest.php';
 
-echo "batata";
-
-//$tester = new RequestTester();
+$tester = new RequestTester();
 
 //$tester->testInsertClient();
 //$tester->testInsertClientwithoutChannel();
 //$tester->testListAllClients();
-//$tester->testAutoRegister();
+$tester->testAutoRegister();
