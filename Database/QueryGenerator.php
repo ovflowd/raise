2f26f4434eb2bc3bdb41465624848bc727ce0598
Lucas Meine <joaolucas.meine@redes.unb.br>
2017-03-18 00:36:15 -0300

    Devmind commit in 2017-03-18 00:36:32

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -58,7 +58,7 @@ Class QueryGenerator
           $queryStr = "SELECT * FROM `".$request->bucket."` WHERE";
           foreach ($request->getParameters() as $key => $parameter)
           {
-              $queryStr = $queryStr . " " . $key . " LIKE \$$key" . "AND ";
+              $queryStr = $queryStr . " " . $key . " = \$$key" . "AND ";
           }
           $request->string = substr($queryStr, 0, -4);
         }
