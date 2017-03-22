7a7b6630bfb0822ff5e5236c9d3dca2d56c9ca75
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-03-22 11:59:26 -0300

    Update Config.php
    Não precisa da porta.

--- Config/Config.php
+++ Config/Config.php
@@ -1,4 +1,4 @@
 <?php
 
 const DB_TYPE = 'COUCHBASE';
-const DB_ADDRESS = '127.0.0.1:8091,user,pass';//enter yours user and password conchbase
+const DB_ADDRESS = '127.0.0.1,user,pass';//enter yours user and password conchbase
