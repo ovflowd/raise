f1202347d6a8a00e6b9972549149fdfbd1b50a87
Nicolas Meinen <nick.meinen@live.com>
2017-04-04 08:25:21 -0300

    Devmind commit in 2017-04-04 08:25:46

--- Database/DatabaseParser.php
+++ Database/DatabaseParser.php
@@ -39,7 +39,7 @@ class DatabaseParser
         if($bucket !== NULL)
         {
           $cluster = new CouchbaseCluster($this->serverAddress);
-          var_dump($cluster);
+          var_dump($cluster->openBucket($bucket));
           return $cluster->openBucket($bucket);
         }
         return $this->bucket;
