c2b68d30bbdf2bcdb465bf10617892798de3be6c
Nicolas Meinen <nick.meinen@live.com>
2017-04-04 19:10:28 -0300

    Devmind commit in 2017-04-04 19:10:54

--- Config/Config.php
+++ Config/Config.php
@@ -1,7 +1,7 @@
 <?php
 
 const DB_TYPE = 'COUCHBASE';
-const DB_ADDRESS = '127.0.0.1,user,pass';//enter yours user and password conchbase
+const DB_ADDRESS = '127.0.0.1,couchadmin,uFuJF*e@3!tdEmiRWh$9';//enter yours user and password conchbase
 const DB_IP= '127.0.0.1';
 const DB_USER = 'user';
 const DB_PASSWORD = 'password';
