2faf70708a6eac0334d5b58aacf0d85494466be1
Nicolas Meinen <nick.meinen@live.com>
2017-04-04 09:22:55 -0300

    Devmind commit in 2017-04-04 09:23:21

--- Database/DatabaseParser.php
+++ Database/DatabaseParser.php
@@ -124,7 +124,7 @@ class DatabaseParser
             return $this->response($this->parseResult($this->getBucket($requestObj->bucket)->query($query) , $requestObj));
         } catch(CouchbaseException $e)
         {
-            return (new MessageOutPut())->messageHttp($e);
+            return (new MessageOutPut())->messageHttp($e->getCode());
         }
     }
 }
