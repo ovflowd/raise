815c0f8588b9e3bc93eb16e046ad33c542894193
Nicolas Meinen <nick.meinen@live.com>
2017-04-04 19:41:10 -0300

    Devmind commit in 2017-04-04 19:41:36

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -83,6 +83,9 @@ Class QueryGenerator
                 }
             }
             echo $queryStr.'<br>'; 
+            if (count($request->getParameters()) === 1 && array_key_exists($request->getParameters(), "tokenId")){
+                $request->string = substr($queryStr, 0, -9);    
+            }
             $request->string = substr($queryStr, 0, -4);
         }
         else
