d57dbe74d46b00ed0f6bd0fee6273701049873c4
Nicolas Meinen <nick.meinen@live.com>
2017-04-04 07:02:23 -0300

    Devmind commit in 2017-04-04 07:02:49

--- Treaters/RequestTreater.php
+++ Treaters/RequestTreater.php
@@ -72,6 +72,7 @@ class RequestTreater
     {
         
         $request = $this->create();
+        var_dump($request);
         $this->validate($request);
         if (!$request->isValid()) {
             return (new MessageOutPut())->messageHttp($request->getReponseCode());
