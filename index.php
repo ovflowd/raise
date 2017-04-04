9d8c753ef52839d61e704efa189edf20d17a3b78
Nicolas Meinen <nick.meinen@live.com>
2017-04-04 19:29:12 -0300

    Devmind commit in 2017-04-04 19:29:38

--- index.php
+++ index.php
@@ -8,6 +8,6 @@ use Raise\Treaters\RequestTreater;
 
 $t = new RequestTreater();
 
-echo '<br>'."hey"; 
+echo '<br>'."coco"; 
 
 //echo json_encode($t->execute(),JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
