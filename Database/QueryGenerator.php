e8f927e4efa8b6928e9a4e39cfe33c1bc504997b
Nicolas Meinen <nick.meinen@live.com>
2017-04-04 20:05:27 -0300

    Devmind commit in 2017-04-04 20:05:54

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -84,9 +84,10 @@ Class QueryGenerator
             }
             
             if (count($request->getParameters()) === 1 && array_key_exists("tokenId", $request->getParameters())){ 
-                echo substr($queryStr, 0, -32);
+                $request->string =  substr($queryStr, 0, -32);
+            } else {
+                $request->string = substr($queryStr, 0, -4);    
             }
-            $request->string = substr($queryStr, 0, -4);
         }
         else
         {
