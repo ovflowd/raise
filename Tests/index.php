74a03095a05a6e2f6008963b8078acb2fc3bb537
Nicolas Meinen <nick.meinen@live.com>
2017-04-04 15:02:29 -0300

    Devmind commit in 2017-04-04 15:02:55

--- Tests/index.php
+++ Tests/index.php
@@ -8,4 +8,7 @@ $tester = new RequestTester();
 //$tester->testInsertClient(); 
 //$tester->testInsertClientwithoutChannel();
 //$tester->testListAllClients();
-$tester->testAutoRegister();
+
+$tester->registerServices("4c9adfb96a364c6805b28f90a342b65c");
+
+//$tester->testAutoRegister();
