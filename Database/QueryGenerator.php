adff1c71339696f58709195f979df946d7f7b4cc
Nicolas Meinen <nick.meinen@live.com>
2017-04-04 09:12:42 -0300

    Devmind commit in 2017-04-04 09:13:07

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -142,6 +142,31 @@ Class QueryGenerator
         if (!empty($method)) 
         {
             
+            if ($request->getPath() ['bucket'] === "client" && $request->getPath() ["method"] !== "register") 
+            {
+               $request->bucket = "token";
+                $parser = new DatabaseParser($request);
+                $token = $request->getParameters() ['tokenId'];
+                $request->string = 'SELECT * FROM `token` WHERE tokenId = $token';
+                $request->setParameters(array(
+                    'token' => $token
+                ));
+                $result = $parser->select($request);
+                $request = $this->validateToken($result, $request);
+                
+                if ($result["code"] === 200 && count($result["values"]) > 0) 
+                {
+                    $request->setResponseCode(200);
+                    $request->setValid(true);
+                    $request->bucket = "data";
+                }
+                else
+                {
+                    $request->setResponseCode(403);
+                    $request->setValid(false);
+                }
+            }
+            
             //if ($request->getPath() ['bucket'] === "data" && $request->getPath() ["method"] !== "register") 
             //{
             //   $request->bucket = "token";
