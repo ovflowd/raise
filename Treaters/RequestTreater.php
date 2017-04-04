b3d8ec43d55efe87348e64f2d2afd8a000652b65
Nicolas Meinen <nick.meinen@live.com>
2017-04-04 10:51:02 -0300

    Devmind commit in 2017-04-04 10:51:28

--- Treaters/RequestTreater.php
+++ Treaters/RequestTreater.php
@@ -169,7 +169,8 @@ class RequestTreater
             $request->setResponseCode(400);
             $request->setValid(false);
             return false;  
-        } else if(count(array_diff(array_keys($request->getParameters()), array_keys((array) $parameters))) === 1) {
+        } else if(count(array_diff(array_keys($request->getParameters()), array_keys((array) $parameters))) === 1
+                  && !in_array("tokenId", array_diff(array_keys($request->getParameters()), array_keys((array) $parameters)))) {
             
         }
         return true;
