938722921d50675fe3552aefc4bf4b41b47849dc
Nicolas Meinen <nick.meinen@live.com>
2017-04-04 07:43:13 -0300

    Devmind commit in 2017-04-04 07:43:39

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -54,7 +54,7 @@ Class QueryGenerator
 
     private function buildQuery($request)
     {
-        if(count($request->getParameters())>0 || (count($request->getParameters()) === 1 && array_key_exists("tokenId",$request->getParameters()) ))
+        if(count($request->getParameters())>0 || !(count($request->getParameters()) === 1 && array_key_exists("tokenId",$request->getParameters()) ))
         {
           $queryStr = "SELECT * FROM `".$request->bucket."` WHERE";
           $typeVerification = array();
