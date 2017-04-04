511a514d7481bec75237d2ab6693406516780e60
Nicolas Meinen <nick.meinen@live.com>
2017-04-04 10:37:57 -0300

    Devmind commit in 2017-04-04 10:38:23

--- Treaters/RequestTreater.php
+++ Treaters/RequestTreater.php
@@ -166,7 +166,7 @@ class RequestTreater
     private function validationMethodGet($request, $parameters)
     {
         if (!empty(array_diff(array_keys($request->getParameters()), array_keys((array) $parameters)))) {
-            if (!in_array(array_diff(array_keys($request->getParameters()), array_keys((array) $parameters)), "tokenId"))
+            if (!in_array("tokenId", array_diff(array_keys($request->getParameters()), array_keys((array) $parameters))))
             {
                 $request->setResponseCode(400);
                 $request->setValid(false);
