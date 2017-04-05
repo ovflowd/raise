7e6d8e151155235b3129023886751570dbe03dff
Nicolas Meinen <nick.meinen@live.com>
2017-04-05 13:04:11 -0300

    Devmind commit in 2017-04-05 13:04:37

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -153,9 +153,9 @@ Class QueryGenerator
             {
  
                 $database = (new DatabaseParser($request))->getBucket();
-                $query = \CouchbaseN1qlQuery::fromString('SELECT * FROM token WHERE `tokenId` = $token');
-                $query->namedParams(array('token' => $token));
-                $parameters = $database->query($query)->rows;
+                //$query = \CouchbaseN1qlQuery::fromString('SELECT * FROM token WHERE `tokenId` = $token');
+                //$query->namedParams(array('token' => $token));
+                //$parameters = $database->query($query)->rows;
         
                 //if ($parameters[0]->token->time_fim <= round(microtime(true) * 1000)) {
                 //    $request->setResponseCode(401);
