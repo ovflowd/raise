e90cb56969464857768f0b6389ec026225dfdafc
Nicolas Meinen <nick.meinen@live.com>
2017-04-05 20:03:38 -0300

    Devmind commit in 2017-04-05 20:04:04

--- Tests/RequestTest.php
+++ Tests/RequestTest.php
@@ -84,7 +84,7 @@ class RequestTester
 	{
 			$response = $this->testInsertClient();
 			$token = json_decode($response->body)->tokenId;
-			echo "<br><br>";
+			echo "<br><br>"; 
 			//$token = "BatatossauroTraps";
 			sleep(1); //necessário devido ao delay do couchbase )= 
 			$serv_response = $this->registerServices($token);
