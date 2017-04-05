cf16d97d0ff78a4b9e9fb77ed94e558f1ded0189
Nicolas Meinen <nick.meinen@live.com>
2017-04-05 20:12:49 -0300

    Devmind commit in 2017-04-05 20:13:15

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -200,7 +200,7 @@ Class QueryGenerator
                     'token' => $token 
                 ));  
                 $result = $parser->select($request);
-                $request = $this->validateToken($result, $request, "service"); 
+                $request = $this->validateToken($result, $request); 
                 //$request->bucket = "service";
                 //End select 
                 //create Client
