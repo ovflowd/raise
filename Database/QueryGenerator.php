9eda65b1c326f943309d3b19622635dec33cb938
Nicolas Meinen <nick.meinen@live.com>
2017-04-05 13:09:23 -0300

    Devmind commit in 2017-04-05 13:09:49

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -164,7 +164,7 @@ Class QueryGenerator
                 } else {
                     $request->setResponseCode(200); 
                     $request->setValid(true);
-                    $request->bucket = "client";
+                    $request->bucket = "client"; 
                 }                         
             }
             
