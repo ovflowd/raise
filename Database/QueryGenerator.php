2fca7c8f91bbb8beb9b62a1721f15cd23fe11e90
Nicolas Meinen <nick.meinen@live.com>
2017-04-05 12:50:13 -0300

    Devmind commit in 2017-04-05 12:50:39

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -161,6 +161,13 @@ Class QueryGenerator
                 ));
                 $result = $parser->select($request);
                 
+                $parameters = $database->query($query)->rows;
+
+                if ($parameters[0]->token->time_fim <= round(microtime(true) * 1000)) {
+                    $request->setResponseCode(401);
+                    $request->setValid(false);
+                    return false;
+                }
                 if ($result["code"] === 200 && count($result["values"]) > 0) 
                 {
                     $request->setParameters($oldParameters);
