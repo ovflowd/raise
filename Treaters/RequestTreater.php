99edbaf58167b5765588cc5e14d310950128e9d8
Nicolas Meinen <nick.meinen@live.com>
2017-04-05 12:48:23 -0300

    Devmind commit in 2017-04-05 12:48:49

--- Treaters/RequestTreater.php
+++ Treaters/RequestTreater.php
@@ -201,7 +201,6 @@ class RequestTreater
             $query->namedParams(array('token' => $token));
             $parameters = $database->query($query)->rows;
 
-            $compare = $parameters[0];
             var_dump($compare); 
             $compare = json_decode(json_encode($compare), true);
             $compare = $compare['services'][$service['service_id']]['parameters'];
