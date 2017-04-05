e8df0836793beb30ebc8e13fb51a5d05dc6a435b
Nicolas Meinen <nick.meinen@live.com>
2017-04-05 13:18:00 -0300

    Devmind commit in 2017-04-05 13:18:26

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -156,7 +156,7 @@ Class QueryGenerator
             
             if ($request->getPath() ['bucket'] === "client" && $request->getPath() ["method"] !== "register") 
             {
-                echo validateExpirationToken($request);
+                //echo validateExpirationToken($request);
                 $token = $request->getParameters() ['tokenId'];
                 $database = (new DatabaseParser($request))->getBucket();
                 $query = \CouchbaseN1qlQuery::fromString('SELECT * FROM token WHERE `tokenId` = $token');
