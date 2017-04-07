0938a96eb41d18ee8dde7cbcb5c8c51a3ce808fe
Nicolas Meinen <nick.meinen@live.com>
2017-04-07 12:39:04 -0300

    Devmind commit in 2017-04-07 12:39:31

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -209,8 +209,9 @@ Class QueryGenerator
             {
                 $request->token = $request->getBody() ['token'];
                 $arrayTest = $request->getBody();  
-                if (validateExpirationToken($request, $request->token)){
+                
                     echo 'here>';
+                if (validateExpirationToken($request, $request->token)){
                     unset($arrayTest ["token"]);    
                     $finalArray = $arrayTest ["values"][0];  
                     $request->treatedBody = json_encode($finalArray);
