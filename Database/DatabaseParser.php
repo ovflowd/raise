4613fa8e819ba3f50fd6985c4765f15719afe581
Nicolas Meinen <nick.meinen@live.com>
2017-04-07 13:33:45 -0300

    Devmind commit in 2017-04-07 13:34:12

--- Database/DatabaseParser.php
+++ Database/DatabaseParser.php
@@ -77,7 +77,7 @@ class DatabaseParser
 
     private function treatData($untreatedResp)
     {
-        echo $untreatedResp["values"]["token"];
+        echo $untreatedResp["values"][0]["token"];
         exit; 
         return $untreatedResp; 
     }
