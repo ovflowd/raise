f00e494550a073747b1e565f333d38b4c04db3f9
Nicolas Meinen <nick.meinen@live.com>
2017-04-07 10:28:07 -0300

    Devmind commit in 2017-04-07 10:28:34

--- Database/DatabaseParser.php
+++ Database/DatabaseParser.php
@@ -100,9 +100,9 @@ class DatabaseParser
         try
         {
               //if ($requestObj->bucket === "client"){ //Vai updatar o client 
-                  $result = $this->getBucket($requestObj->bucket)->upsert($requestObj->token, $requestObj->treatedBody);
+              //    $result = $this->getBucket($requestObj->bucket)->upsert($requestObj->token, $requestObj->treatedBody);
               //} else { // Inserir novas coisas 
-              //    $result = $this->getBucket($requestObj->bucket)->insert($requestObj->token, $requestObj->treatedBody);
+                  $result = $this->getBucket($requestObj->bucket)->insert($requestObj->token, $requestObj->treatedBody);
               //}
               $result->token = $requestObj->token;
               $result->method = $requestObj->getPath()['method'];
