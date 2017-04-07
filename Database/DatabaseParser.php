86cf8392e9934c3f5690dac2ca3afac831983117
Nicolas Meinen <nick.meinen@live.com>
2017-04-07 13:08:07 -0300

    Devmind commit in 2017-04-07 13:08:35

--- Database/DatabaseParser.php
+++ Database/DatabaseParser.php
@@ -70,7 +70,7 @@ class DatabaseParser
                 'code' => 200,
                 'values' => $responseRows
             );
-        echo($responseRows);
+        echo($responseRows[0]);
         exit;
         if (isset($responseRows[0]["token"])){
                 echo "entrou";
