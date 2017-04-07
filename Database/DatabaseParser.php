aecadef9afe1f80c9c8ed811a98a39b9b59ca939
Nicolas Meinen <nick.meinen@live.com>
2017-04-07 10:35:46 -0300

    Devmind commit in 2017-04-07 10:36:14

--- Database/DatabaseParser.php
+++ Database/DatabaseParser.php
@@ -99,7 +99,7 @@ class DatabaseParser
     { 
         try
         {
-              if ($requestObj->bucket === "data"){ //Vai updatar o client 
+              if ($requestObj->bucket === "data"){ //Vai updatar o client  
                 $result = $this->getBucket($requestObj->bucket)->insert(round(microtime(true), $requestObj->treatedBody);
               } else { // Inserir novas coisas  
                 $result = $this->getBucket($requestObj->bucket)->upsert($requestObj->token, $requestObj->treatedBody);
