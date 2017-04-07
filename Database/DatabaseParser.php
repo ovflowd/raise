45db05c52b6a348853de3d6a1f12e41724578c27
Nicolas Meinen <nick.meinen@live.com>
2017-04-07 13:09:26 -0300

    Devmind commit in 2017-04-07 13:09:53

--- Database/DatabaseParser.php
+++ Database/DatabaseParser.php
@@ -48,7 +48,6 @@ class DatabaseParser
     {
         if (isset($responseRows->cas))
         { 
-            echo "here"; 
             $response = (new MessageOutPut())->messageHttp(200);
             if($responseRows->bucket === "client" || $responseRows->bucket === "service")
             { 
