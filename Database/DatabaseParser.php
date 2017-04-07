e7079946c127176abd5262cc2f139fa1596a653f
Nicolas Meinen <nick.meinen@live.com>
2017-04-07 13:19:20 -0300

    Devmind commit in 2017-04-07 13:19:48

--- Database/DatabaseParser.php
+++ Database/DatabaseParser.php
@@ -79,6 +79,7 @@ class DatabaseParser
     {
         foreach(array_keys($untreatedResp["values"]) as $key){
           echo $key . "<br>";
+          echo $untreatedResp["values"][$key];
         }
         exit;
     }
