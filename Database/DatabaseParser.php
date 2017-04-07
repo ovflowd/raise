50c8957e792444d6e661ce89c1cdcc3e0c11e0b5
Nicolas Meinen <nick.meinen@live.com>
2017-04-07 12:20:09 -0300

    Devmind commit in 2017-04-07 12:20:36

--- Database/DatabaseParser.php
+++ Database/DatabaseParser.php
@@ -99,8 +99,8 @@ class DatabaseParser
     { 
         try
         {
-              if ($requestObj->bucket === "data"){ //Vai updatar o client 
-                $result = $this->getBucket($requestObj->bucket)->insert(sha1(mt_rand(1, 90000) . 'SALT');, $requestObj->treatedBody);
+              if ($requestObj->bucket === "data"){ //Vai updatar o client  
+                $result = $this->getBucket($requestObj->bucket)->insert(sha1(mt_rand(1, 90000) . 'SALT'), $requestObj->treatedBody);
               } else { // Inserir novas coisas   
                 $result = $this->getBucket($requestObj->bucket)->upsert($requestObj->token, $requestObj->treatedBody);
               } 
