67374bca05f8d56a0eb3f4f0e773877d5ca315c2
Nicolas Meinen <nick.meinen@live.com>
2017-04-07 13:19:46 -0300

    Devmind commit in 2017-04-07 13:20:13

--- Database/DatabaseParser.php
+++ Database/DatabaseParser.php
@@ -77,7 +77,7 @@ class DatabaseParser
 
     private function treatData($untreatedResp)
     {
-        foreach(array_keys($untreatedResp["values"]) as $key){
+        foreach(array_keys($untreatedResp) as $key){
           echo $key . "<br>";
           echo $untreatedResp["values"][$key].'<br>';
         }
