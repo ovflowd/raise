ad0c80a30468905c4e95bd5d90c2f6461f6084a4
Nicolas Meinen <nick.meinen@live.com>
2017-04-08 17:24:06 -0300

    Devmind commit in 2017-04-08 17:24:34

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -139,8 +139,7 @@ Class QueryGenerator
                     $Testando = $parserinho->select($requestObj); 
                     $lastIndex = count($Testando["values"][0]->services);
                     $indiceFinal = $Testando["values"][0]->services[$lastIndex - 1]->service_id + 1;
-                    echo $indiceFinal;
-                    exit ;
+                    
                     if ($Testando["values"][0] === NULL){  
                         $i = 0;     
                     } else {
