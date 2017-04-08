8a080fde1198a89d34096563d3067f4066ca1d5a
Nicolas Meinen <nick.meinen@live.com>
2017-04-08 16:35:37 -0300

    Devmind commit in 2017-04-08 16:36:05

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -128,11 +128,11 @@ Class QueryGenerator
                 $request->service = true;
                 $services = array();
                 
-                if ($nextBucket == "service"){
+                if ($nextBucket == "service" || $nextBucket == "client" ){
                     $requestObj = $request;
                     $parserinho = new DatabaseParser($this->parsePath($requestObj, false) , true);
                     $requestObj->string = "select * from service order by service desc limit 1";
-                    $requestObj->bucket = "service";  
+                    $requestObj->bucket = "service";   
                     $Testando = $parserinho->select($requestObj); 
                     $lastIndex = count($Testando["values"][0]->services);
                     $indiceFinal = $Testando["values"][0]->services[$lastIndex - 1]->service_id + 1;
