43798253d600cb566540928084f1c26386b5f396
Nicolas Meinen <nick.meinen@live.com>
2017-04-10 19:34:23 -0300

    Devmind commit in 2017-04-10 19:34:53

--- Tests/index.php
+++ Tests/index.php
@@ -9,6 +9,10 @@ $tester = new RequestTester();
 //$tester->testInsertClientwithoutChannel();
 //$tester->testListAllClients();
 $tester->testAutoRegister(); 
-echo '<br><br><br>'."Testando POST ClientInvalido".'<br><br>';
+echo '<br><br><br>'."Testando POST Client Invalido".'<br><br>';
 $tester->invalidPostTest();
+echo '<br><br><br>'."Testando POST Client Invalido".'<br><br>';
+$tester->invalidPostTest();
+
+
 //$tester->registerServices("80ad313c43dbfe095cc5d76c4029f499");
