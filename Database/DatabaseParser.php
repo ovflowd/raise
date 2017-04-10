ad763f3743936d32824ae0264d950b8d72769ef3
Nicolas Meinen <nick.meinen@live.com>
2017-04-10 09:29:44 -0300

    Devmind commit in 2017-04-10 09:30:13

--- Database/DatabaseParser.php
+++ Database/DatabaseParser.php
@@ -82,7 +82,7 @@ class DatabaseParser
     private function treatData($untreatedResp)
     { 
         for ($i = 0; $i < count($untreatedResp["values"]); $i ++){
-            unset($untreatedResp["values"][$i]->token); 
+            unset($untreatedResp["values"][$i]->token);  
         /*  unset($untreatedResp["values"][$i]->token); 
             for ($j = 0 ; $j < count($dataValues = $untreatedResp["values"][$i]->data); $j ++)
             {
