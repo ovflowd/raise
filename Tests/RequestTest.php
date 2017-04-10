40b8b52fd4d5cb8572b8766311594ac80dbce330
Nicolas Meinen <nick.meinen@live.com>
2017-04-10 12:53:39 -0300

    Devmind commit in 2017-04-10 12:54:08

--- Tests/RequestTest.php
+++ Tests/RequestTest.php
@@ -95,7 +95,7 @@ class RequestTester
 			foreach (json_decode($serv_response)->services as $key => $service){
 			    $aServicesId[$key] = json_decode($serv_response)->services[$key]->service_id;
 			}
-			exit;
+			
 			$dados = [ 'token' => $token,  "data" => (array((array('service_id' => 0 , 'data_values' => array('ambiguous'=>true)))))];
 			echo "<br><br>";
 			$postData = $this->postData($dados); 
