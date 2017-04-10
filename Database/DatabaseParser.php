1d9504cf8e586b299253862370fa774e4e145745
Nicolas Meinen <nick.meinen@live.com>
2017-04-09 21:40:36 -0300

    Devmind commit in 2017-04-09 21:41:04

--- Database/DatabaseParser.php
+++ Database/DatabaseParser.php
@@ -61,7 +61,7 @@ class DatabaseParser
                       
                     foreach(json_decode($responseRows->request->treatedBody)->services as $key=>$service)
                     {
-                        var_dump(json_encode($responseRows->request->treatedBody));
+                        var_dump(json_decode(json_encode($responseRows->request->treatedBody), true_);
                         exit;
                         $response->services[] = array('service_id' => $key, 'service_name' => $service->name);
                     }
