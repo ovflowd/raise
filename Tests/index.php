f0f3b6b230ce0d69af3eb1dbd5afbe6d9b47f584
Nicolas Meinen <nick.meinen@live.com>
2017-04-10 19:39:01 -0300

    Devmind commit in 2017-04-10 19:39:30

--- Tests/index.php
+++ Tests/index.php
@@ -12,5 +12,5 @@ $token = $tester->testAutoRegister();
 echo '<br><br><br>'."Testando POST Client Invalido".'<br><br>';
 $tester->invalidClientPostTest();
 echo '<br><br><br>'."Testando POST Service Invalido".'<br><br>';
-$tester->invalidServicePostTest("0d16bc583ddc9f2c3c7ab1350c67605b"); 
+$tester->invalidServicePostTest($token); 
 //$tester->registerServices("80ad313c43dbfe095cc5d76c4029f499");
