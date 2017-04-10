844f77f86e8f32657e7be725be94a4796aaf933c
Nicolas Meinen <nick.meinen@live.com>
2017-04-10 19:30:10 -0300

    Devmind commit in 2017-04-10 19:30:39

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -245,12 +245,7 @@ class QueryGenerator
                     $request->setResponseCode(400);  
                     $request->setValid(false);
                     return $request;
-                } else{
-                    var_dump(json_encode($request->getBody())); 
-                    $teste = "hey";
-                    var_dump($teste);
-                    exit;
-                }   
+                }  
                 $request->bucket = 'token';
                 $request->token = $this->generateToken();
                 $tokenIni = round(microtime(true) * 1000);
