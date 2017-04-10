ccd867474ecbb856560aef3b17a8d7bc2c4b15e3
Nicolas Meinen <nick.meinen@live.com>
2017-04-09 22:39:37 -0300

    Devmind commit in 2017-04-09 22:40:06

--- Database/DatabaseParser.php
+++ Database/DatabaseParser.php
@@ -142,7 +142,7 @@ class DatabaseParser
         {  
             $query = \CouchbaseN1qlQuery::fromString($requestObj->string); 
             $query->namedParams($requestObj->getParameters());
-            echo $query;
+            var_dump($query);
             exit;
             $untreatedResp = $this->response($this->parseResult($this->getBucket($requestObj->bucket)->query($query) , $requestObj));
             if ($requestObj->bucket === "data"){ 
