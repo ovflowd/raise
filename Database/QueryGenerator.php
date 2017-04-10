8595f0b1b9da037abe5ba6b87add6de1b5b5f313
Nicolas Meinen <nick.meinen@live.com>
2017-04-10 09:23:00 -0300

    Devmind commit in 2017-04-10 09:23:29

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -109,7 +109,7 @@ Class QueryGenerator
                 }
                 else if ($request->bucket == "data" && $key == "service_id" ) 
                 {  
-                    $chave = "data" . $key;
+                    $chave = "data.data" . $key;
                     //$queryStr = "SELECT * FROM data data UNNEST data.data c WHERE c.service_id = "."\$$key"." AND";
                     //$jumpCondition = true;
                 }
