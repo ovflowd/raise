1f9b7f983bdacf30f44d8e9e5ff0306630c434c0
Nicolas Meinen <nick.meinen@live.com>
2017-04-10 19:46:41 -0300

    Devmind commit in 2017-04-10 19:47:10

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -237,7 +237,7 @@ class QueryGenerator
                     $request->setResponseCode(200);
                     $request->setValid(true);
                 }
-            } else {
+            } else { //Mini JSON validation 
                 if (json_encode($request->getBody()) == 'null' || $this->isJson(json_encode($request->getBody())) === 0 ||
                     substr(json_encode($request->getBody()), 0, 1) != "{") {
                     $request->setResponseCode(400);  
