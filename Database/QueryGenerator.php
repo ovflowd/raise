2434a1511b8a6baf3acd8965d2ad3b3eb9fd4bac
Nicolas Meinen <nick.meinen@live.com>
2017-04-10 13:30:52 -0300

    Devmind commit in 2017-04-10 13:31:20

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -81,7 +81,7 @@ class QueryGenerator
     
     private function validateId($request, $serviceId)
     {
-        $Testando = $this->simpleSelect($request, 'service', "SELECT * FROM service serv UNNEST serv.services c WHERE c.service_id = 1");
+        $Testando = $this->simpleSelect($request, 'service', "SELECT * FROM service serv UNNEST serv.services c WHERE c.service_id = 28");
         var_dump ($Testando);
         exit;  
     }  
