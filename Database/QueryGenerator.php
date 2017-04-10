7f5ff71ab6113d68461db20016c0c0301f7067ba
Nicolas Meinen <nick.meinen@live.com>
2017-04-10 19:30:35 -0300

    Devmind commit in 2017-04-10 19:31:04

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -242,6 +242,7 @@ class QueryGenerator
             if ($request->getPath() ['bucket'] === 'client' && $request->getPath() ['method'] == 'register') {
                 if (json_encode($request->getBody()) == 'null' || $this->isJson(json_encode($request->getBody())) === 0 ||
                     substr(json_encode($request->getBody()), 0, -1) != "{") {
+                    echo substr(json_encode($request->getBody()), 0, -1);
                     $request->setResponseCode(400);  
                     $request->setValid(false);
                     return $request;
