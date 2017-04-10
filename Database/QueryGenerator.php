45d0c6be49032b9936a4678347c52ea0eac130c8
Nicolas Meinen <nick.meinen@live.com>
2017-04-09 21:59:19 -0300

    Devmind commit in 2017-04-09 21:59:48

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -221,7 +221,7 @@ Class QueryGenerator
             if ($request->getPath() ['bucket'] === "client" && $request->getPath() ['method'] == "register") 
             {
                 echo json_encode($request->getBody()).'<br>';
-                if (json_encode($request->getBody()) == null){
+                if (json_encode($request->getBody()) == "null"){
                     echo "entrou no certo";
                     $request->setResponseCode(400); 
                     $request->setValid(false);  
