8388b0247283a6e119ee816bf304120dd4b249e0
Nicolas Meinen <nick.meinen@live.com>
2017-04-09 21:53:02 -0300

    Devmind commit in 2017-04-09 21:53:31

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -224,7 +224,7 @@ Class QueryGenerator
                 $request->token = $this->generateToken();
                 $tokenIni = round(microtime(true) * 1000);
                 $tokenFim = $tokenIni + 7200000; //millisecons
-                $request->treatedBody = json_encode(array_merge($request->getBody() , array(
+                $request->treatedBody = json_encode(array_merge($request->getBody(), array(
                     'tokenId' => $request->token,
                     'time_ini' => $tokenIni,
                     'time_fim' => $tokenFim
