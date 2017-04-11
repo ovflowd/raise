2617c7f0b82e2daf36c27fd0d0937c0905e3c59b
Nicolas Meinen <nick.meinen@live.com>
2017-04-11 00:20:05 -0300

    Devmind commit in 2017-04-11 00:20:34

--- Treaters/RequestTreater.php
+++ Treaters/RequestTreater.php
@@ -196,6 +196,7 @@ class RequestTreater
         $parameters = $database->query($query)->rows;
 
         if ($parameters[0]->token->time_fim <= round(microtime(true) * 1000)) {
+            echo " not"
             $request->setResponseCode(401); 
             $request->setValid(false);
             return false;
