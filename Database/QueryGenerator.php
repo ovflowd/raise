c3a0ab11942904070bd51cb8d020535c0a79cb09
Nicolas Meinen <nick.meinen@live.com>
2017-04-11 00:32:04 -0300

    Devmind commit in 2017-04-11 00:32:33

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -294,7 +294,7 @@ class QueryGenerator
                     $token = $request->getBody()['tokenId'];
                    //select do token id 
                    $queryStr = "SELECT * FROM service WHERE tokenId = '$token'"; 
-                   exit ($queryStr);
+                   $this->simpleSelect();
                 }
             } elseif ($request->getPath() ['bucket'] === 'data' && $request->getPath() ['method'] == 'register') {
                 $request->token = $request->getBody() ['token'];
