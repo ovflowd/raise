0681cd69d1e99fc41942969ec0311699a587f34e
Nicolas Meinen <nick.meinen@live.com>
2017-04-10 23:56:38 -0300

    Devmind commit in 2017-04-10 23:57:07

--- Treaters/RequestTreater.php
+++ Treaters/RequestTreater.php
@@ -221,6 +221,7 @@ class RequestTreater
     private function validationMethodPost($request, $parameters)
     {
         if (!empty(array_diff(array_keys((array) $parameters), array_keys($request->getBody())))) {
+            var_dump(array_diff(array_keys((array) $parameters), array_keys($request->getBody())));
             $request->setResponseCode(400);
             $request->setValid(false);
 
