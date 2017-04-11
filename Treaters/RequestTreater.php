36998836e105750824f0531bd03355602b45d0b0
Nicolas Meinen <nick.meinen@live.com>
2017-04-11 00:16:54 -0300

    Devmind commit in 2017-04-11 00:17:24

--- Treaters/RequestTreater.php
+++ Treaters/RequestTreater.php
@@ -109,7 +109,6 @@ class RequestTreater
     private function validate($request)
     {
         if ($this->emptyValidation($request) && $this->validationBucket($request) && $this->validationMethod($request) && $this->validateMethodMoreBucket($request)) {
-            echo "done";
             $request->setResponseCode(200);
             $request->setValid(true);
             return;
