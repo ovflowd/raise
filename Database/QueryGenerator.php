0122c8a9c051d83fdcb4fe1b0838ea16e4d2d8f5
Nicolas Meinen <nick.meinen@live.com>
2017-04-11 00:54:09 -0300

    Devmind commit in 2017-04-11 00:54:38

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -301,7 +301,7 @@ class QueryGenerator
                    
                    $queryStr = "SELECT * FROM client WHERE tokenId = '$oldToken'";
                    $oldTokenDocument = json_encode($this->simpleSelect($request, 'client', $queryStr, null)["values"][0]);
-                   var_dump($oldTokenDocument); 
+                   var_dump(json_decode($oldTokenDocument,false)); 
                    //unset($oldTokenDocument);
                    exit;
                    
