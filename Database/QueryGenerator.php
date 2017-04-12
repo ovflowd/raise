6d8c8e886505677a9a3a0be7688b191c78e84c02
Nicolas Meinen <nick.meinen@live.com>
2017-04-12 02:58:47 -0300

    Devmind commit in 2017-04-12 02:59:17

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -443,6 +443,7 @@ class QueryGenerator
                         $request->treatedBody = json_encode($oldTokenObject);
                         $request->token = $oldTokenObject->tokenId;  
                         $parser->insert($request, false);
+                        unset($oldTokenDocument->treatedBody);
                         //Insere uma nova token valida pro cara
                         $request->bucket = 'token';
                         $request->token = $newDocument->tokenId;
