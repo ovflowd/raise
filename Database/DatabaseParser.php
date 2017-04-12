ab7788f313c2bc064176b7aab7d1caebc8a71dcd
Nicolas Meinen <nick.meinen@live.com>
2017-04-12 02:52:28 -0300

    Devmind commit in 2017-04-12 02:52:58

--- Database/DatabaseParser.php
+++ Database/DatabaseParser.php
@@ -118,10 +118,6 @@ class DatabaseParser
         { 
               if ($requestObj->bucket === "client" || $requestObj->bucket === "token"){ 
                 if (!$isRevalidate){
-                    if ($requestObj->bucket === "token"){
-                        echo "Here Token";    
-                        var_dump($requestObj->treatedBody);
-                    }
                     $result = $this->getBucket($requestObj->bucket)->upsert($requestObj->token, $requestObj->treatedBody);
                 } else {
                     $result = $this->getBucket($requestObj->bucket)->insert($requestObj->token, $requestObj->treatedBody);
