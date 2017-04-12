fbb91a1b8740087f8c0e79579b8a8c8d7cdf79a2
Nicolas Meinen <nick.meinen@live.com>
2017-04-12 02:24:07 -0300

    Devmind commit in 2017-04-12 02:24:37

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -406,7 +406,7 @@ class QueryGenerator
                 $token = $request->getBody() ['tokenId'];
                 $bIsRev = $this->simpleSelect($request, "token", "select * from token where tokenId = '".$token."'", null)["values"][0];
                 if ($bIsRev->is_revalidated){
-                    $request->setResponseCode(400);
+                    $request->setResponseCode(401);
                     $request->setValid(false);
                 }
                 
