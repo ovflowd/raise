340bc6c21e2da1799d0b48a10d64332d5bbe8c30
Nicolas Meinen <nick.meinen@live.com>
2017-04-12 02:33:04 -0300

    Devmind commit in 2017-04-12 02:33:34

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -406,6 +406,7 @@ class QueryGenerator
                 $token = $request->getBody() ['tokenId'];
                 $oldTokenObject = $this->simpleSelect($request, "token", "select * from token where tokenId = '".$token."'", null)["values"][0];
                 if ($oldTokenObject->is_revalidated){
+                    echo "oldAnauth";
                     $request->setResponseCode(401); //Already revalidated
                     $request->setValid(false);
                     return $request;
