641953a5fd433844a67ddcbe139c0756d70889cb
Nicolas Meinen <nick.meinen@live.com>
2017-04-12 03:16:18 -0300

    Devmind commit in 2017-04-12 03:16:47

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -426,12 +426,14 @@ class QueryGenerator
                     $oldToken = $newDocument->tokenId;
                     $newDocument->tokenId = $this->generateToken();
                     //Insere o novo serviço com nova tokenId no service
+                    
                     if (count($validServices)>0){
                         $request->bucket = 'service';
                         $request->treatedBody = json_encode($newDocument);
                         $parser = new DatabaseParser($request, false);
                         $parser->insert($request);
-                    } 
+                    }
+                    
                     $queryStr = "SELECT * FROM client WHERE tokenId = '$oldToken'";
                     $oldTokenDocument = json_decode(json_encode($this->simpleSelect($request, 'client', $queryStr, null) ["values"][0]) , false);
                     $oldClientDocument = $oldTokenDocument;
