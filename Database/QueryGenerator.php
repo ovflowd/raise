6658fb8cd1431c030d0cea3c9c36c208fdd812a2
Nicolas Meinen <nick.meinen@live.com>
2017-04-12 02:27:59 -0300

    Devmind commit in 2017-04-12 02:28:28

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -405,7 +405,7 @@ class QueryGenerator
                 //valida se os serviços enviados fazem parte do token
                 $token = $request->getBody() ['tokenId'];
                 $oldTokenObject = $this->simpleSelect($request, "token", "select * from token where tokenId = '".$token."'", null)["values"][0];
-                if ($bIsRev->is_revalidated){
+                if ($oldTokenObject->is_revalidated){
                     $request->setResponseCode(401); //Already revalidated
                     $request->setValid(false);
                     return $request;
@@ -440,7 +440,7 @@ class QueryGenerator
                         unset($oldTokenDocument->services);
                         unset($oldTokenDocument->tokenId);
                         //Updata a old token para revalidated como true e dá um upsert
-                        $bIsRev->is_revalidated = true;
+                        $oldTokenObject->is_revalidated = true;
                         $request->bucket = 'token';
                         $request->treatedBody = json_encode($bIsRev);
                         $parser->insert($request);
