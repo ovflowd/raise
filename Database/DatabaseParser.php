02f223841067d2ac36edaf65fb0e9a8245e3cd3f
Iury Montenegro <iury2807@hotmail.com>
2017-04-14 10:04:55 -0300

    Devmind commit in 2017-04-14 10:05:26

--- Database/DatabaseParser.php
+++ Database/DatabaseParser.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * UIoT Service Layer
+ * UIoT Service Layer.
+ *
  * @version alpha
  *                          88
  *                          ""              ,d
@@ -16,90 +17,84 @@
  * @copyright University of Brasília
  * important docs http://developer.couchbase.com/documentation/server/current/sdk/php/start-using-sdk.html (will be removed later)
  */
-include_once ("Treaters/MessageOutPut.php");  
-include_once ("opt/RAISe/Config/Config.php"); 
+include_once 'Treaters/MessageOutPut.php';
+include_once 'opt/RAISe/Config/Config.php';
 
 use Raise\Treaters\MessageOutPut;
 use Raise\Treaters\Config;
 
 class DatabaseParser
-{ 
+{
     private $serverAddress;
-    private $bucket; 
- 
-    public function __construct($requestObj, $newBucket)  
+    private $bucket;
+
+    public function __construct($requestObj, $newBucket)
     {
         $this->serverAddress = DB_ADDRESS;
-        if ($simpleQuery !== NULL || $simpleQuery == false){
-            $this->bucket = $this->connect($requestObj->getPath()['bucket'], $this->serverAddress);    
+        if ($simpleQuery !== null || $simpleQuery == false) {
+            $this->bucket = $this->connect($requestObj->getPath()['bucket'], $this->serverAddress);
         } else {
-            $this->bucket = $this->connect($newBucket, $this->serverAddress);        
+            $this->bucket = $this->connect($newBucket, $this->serverAddress);
         }
     }
 
-
-    public function getBucket($bucket = NULL)
+    public function getBucket($bucket = null)
     {
-        if($bucket !== NULL)
-        {
-          $cluster = new CouchbaseCluster($this->serverAddress);
-          return $cluster->openBucket($bucket);
+        if ($bucket !== null) {
+            $cluster = new CouchbaseCluster($this->serverAddress);
+
+            return $cluster->openBucket($bucket);
         }
-        return $this->bucket; 
+
+        return $this->bucket;
     }
 
-    private function response($responseRows = NULL)
+    private function response($responseRows = null)
     {
-        if (isset($responseRows->cas))
-        { 
+        if (isset($responseRows->cas)) {
             $response = (new MessageOutPut())->messageHttp(200);
-            if($responseRows->bucket === "client" || $responseRows->bucket === "service")
-            { 
-                if(isset($responseRows->request->service))
-                { 
+            if ($responseRows->bucket === 'client' || $responseRows->bucket === 'service') {
+                if (isset($responseRows->request->service)) {
                     $response->services = array();
-                      
-                    foreach(json_decode($responseRows->request->treatedBody)->services as $key=>$service)
-                    {
-                        $response->services[] = array('service_id' => json_decode($responseRows->request->treatedBody, true)["services"][$key]["service_id"], 'service_name' => $service->name);
+
+                    foreach (json_decode($responseRows->request->treatedBody)->services as $key => $service) {
+                        $response->services[] = array('service_id' => json_decode($responseRows->request->treatedBody, true)['services'][$key]['service_id'], 'service_name' => $service->name);
                     }
                 }
-              $response->tokenId = $responseRows->token;
+                $response->tokenId = $responseRows->token;
             }
-
-            
-        } else
-        {
+        } else {
             $response = array(
-                'code' => 200, 
-                'values' => $responseRows
+                'code' => 200,
+                'values' => $responseRows,
             );
-        }  
-         
-        return $response; 
+        }
+
+        return $response;
     }
 
     private function treatData($untreatedResp)
-    {  
-        for ($i = 0; $i < count($untreatedResp["values"]); $i ++){
-            unset($untreatedResp["values"][$i]->token);  
-            $dataValues = $untreatedResp["values"][$i]->data->data_values;
-            $serviceId = $untreatedResp["values"][$i]->data->service_id;
-            $untreatedResp["values"][$i]->data_values = $dataValues; 
-            $untreatedResp["values"][$i]->service_id = $serviceId;
-            unset($untreatedResp["values"][$i]->data);
-        } 
-        return $untreatedResp;  
+    {
+        for ($i = 0; $i < count($untreatedResp['values']); ++$i) {
+            unset($untreatedResp['values'][$i]->token);
+            $dataValues = $untreatedResp['values'][$i]->data->data_values;
+            $serviceId = $untreatedResp['values'][$i]->data->service_id;
+            $untreatedResp['values'][$i]->data_values = $dataValues;
+            $untreatedResp['values'][$i]->service_id = $serviceId;
+            unset($untreatedResp['values'][$i]->data);
+        }
+
+        return $untreatedResp;
     }
-    
+
     private function parseResult($result, $request)
     {
         $responseRows = array();
-        foreach ($result->rows as $row) 
-        {
+        foreach ($result->rows as $row) {
             $bucket = $request->bucket;
             $responseRows[] = $row->$bucket;
         }
+
         return $responseRows;
     }
 
@@ -107,51 +102,49 @@ class DatabaseParser
     {
         $cluster = new CouchbaseCluster($serverAddress);
         $bucket = $cluster->openBucket($bucket);
+
         return $bucket;
     }
 
     //Method for performing a insert query on the database.
-    //return string 
+    //return string
     public function insert($requestObj, $isRevalidate)
-    { 
-        try
-        { 
-              if ($requestObj->bucket === "client" || $requestObj->bucket === "token"){ 
-                if (!$isRevalidate){
+    {
+        try {
+            if ($requestObj->bucket === 'client' || $requestObj->bucket === 'token') {
+                if (!$isRevalidate) {
                     $result = $this->getBucket($requestObj->bucket)->upsert($requestObj->token, $requestObj->treatedBody);
                 } else {
                     $result = $this->getBucket($requestObj->bucket)->insert($requestObj->token, $requestObj->treatedBody);
-                }  
-              } else { // Inserir novas coisas       
-                $result = $this->getBucket($requestObj->bucket)->insert(sha1(mt_rand(1, round(microtime(true) * 1000)) . 'SALT'), $requestObj->treatedBody);
-              }  
-              $result->token = $requestObj->token;
-              $result->method = $requestObj->getPath()['method'];
-              $result->bucket = $requestObj->bucket;
-              $result->request = $requestObj;
-              return $this->response($result);    
-        } catch(CouchbaseException $e)  
-        {
+                }
+            } else { // Inserir novas coisas
+                $result = $this->getBucket($requestObj->bucket)->insert(sha1(mt_rand(1, round(microtime(true) * 1000)).'SALT'), $requestObj->treatedBody);
+            }
+            $result->token = $requestObj->token;
+            $result->method = $requestObj->getPath()['method'];
+            $result->bucket = $requestObj->bucket;
+            $result->request = $requestObj;
+
+            return $this->response($result);
+        } catch (CouchbaseException $e) {
             return (new MessageOutPut())->messageHttp($e->getCode());
-        } 
-    } 
-  
-    //Method for performing a select query on the database. 
+        }
+    }
+
+    //Method for performing a select query on the database.
     //return string
-    public function select($requestObj) 
+    public function select($requestObj)
     {
-        try 
-        {  
-            $query = \CouchbaseN1qlQuery::fromString($requestObj->string); 
+        try {
+            $query = \CouchbaseN1qlQuery::fromString($requestObj->string);
             $query->namedParams($requestObj->getParameters());
-            $untreatedResp = $this->response($this->parseResult($this->getBucket($requestObj->bucket)->query($query) , $requestObj));
-            if ($requestObj->bucket === "data"){ 
+            $untreatedResp = $this->response($this->parseResult($this->getBucket($requestObj->bucket)->query($query), $requestObj));
+            if ($requestObj->bucket === 'data') {
                 return $this->treatData($untreatedResp);
-            } else { 
+            } else {
                 return $untreatedResp;
             }
-        } catch(CouchbaseException $e)
-        {
+        } catch (CouchbaseException $e) {
             return (new MessageOutPut())->messageHttp($e->getCode());
         }
     }
