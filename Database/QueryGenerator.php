2a0b087285086b9fa3cf5083d760841e14b6a19f
Nicolas Meinen <nick.meinen@live.com>
2017-04-17 22:30:08 -0300

    Devmind commit in 2017-04-17 22:30:40

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -129,7 +129,7 @@ class QueryGenerator
 
     private function buildQuery($request)
     {
-        $isLimited = false;
+        $request->isLimited = false;
         if (count($request->getParameters()) > 0 && !(count($request->getParameters()) === 1 && array_key_exists('tokenId', $request->getParameters()))) {
             $queryStr = 'SELECT * FROM `'.$request->bucket.'` WHERE';
             if (isset($request->getParameters()['tag'])) {
