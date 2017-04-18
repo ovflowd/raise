e9bd99aa2d1b7a24750859b8e1c4508ccd0b53ed
Nicolas Meinen <nick.meinen@live.com>
2017-04-18 01:44:17 -0300

    Devmind commit in 2017-04-18 01:44:49

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -155,6 +155,7 @@ class QueryGenerator
         } else {
             $request->string = $queryStr = 'SELECT * FROM `'.$request->bucket.'`';
         } 
+        echo $request->getParameters()["limit"];
         echo $request->string;
         return $request;
     }
