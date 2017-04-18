a0eaa8bb6ab5504ab20bf9450681c4462a2eb47e
Nicolas Meinen <nick.meinen@live.com>
2017-04-18 02:02:26 -0300

    Devmind commit in 2017-04-18 02:02:59

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -153,6 +153,7 @@ class QueryGenerator
                 }
             }
              $request->string = $this->finalizeQuery($request, $queryStr, false);
+             echo $request->string;
         } else {
             $request->string = $queryStr = 'SELECT * FROM `'.$request->bucket.'`';
         } 
