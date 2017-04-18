1e112595b7268ac56ac1e45e2cbacf0daebef942
Nicolas Meinen <nick.meinen@live.com>
2017-04-18 03:08:10 -0300

    Devmind commit in 2017-04-18 03:08:42

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -132,7 +132,7 @@ class QueryGenerator
         $request->isLimited = false;
         if (count($request->getParameters()) > 0 && !(count($request->getParameters()) === 1 && array_key_exists('tokenId', $request->getParameters()))) {
             $queryStr = 'SELECT * FROM `'.$request->bucket.'` WHERE';
-            $request = $this->preValidate($queryStr, $request);
+            $request = $this->preValidate($request, $queryStr);
             $queryStr = $request->queryStr; 
             $typeVerification = array();
             foreach ($request->getParameters() as $key => $parameter) {
