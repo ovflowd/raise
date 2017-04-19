a04de4a988e5e6fbc8c564ceb88bb4e224587309
Nicolas Meinen <nick.meinen@live.com>
2017-04-18 23:24:52 -0300

    Devmind commit in 2017-04-18 23:25:25

--- Database/DatabaseParser.php
+++ Database/DatabaseParser.php
@@ -93,6 +93,7 @@ class DatabaseParser
         foreach ($result->rows as $row) {
             $bucket = $request->bucket;
             $responseRows[] = $row->$bucket;
+            echo 'rowbucket:'.'<br>';
             var_dump ($row->$bucket);
         }
         var_dump($responseRows);
