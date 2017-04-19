6ff37c51bbb9d4aad1a2d0a3899a5882e3f87a88
Nicolas Meinen <nick.meinen@live.com>
2017-04-19 03:31:53 -0300

    Devmind commit in 2017-04-19 03:32:27

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -374,7 +374,7 @@ class QueryGenerator
                     'time_ini' => $tokenIni,
                     'time_fim' => $tokenFim,
                     'is_revalidated' => false,
-                )));
+                ))); 
                 $parser = new DatabaseParser($request, false);
                 $parser->insert($request);  
                 $request->bucket = 'client'; 
