f2e878bb09029984f08e94b7e9f0a779104ee474
Nicolas Meinen <nick.meinen@live.com>
2017-04-19 00:43:58 -0300

    Devmind commit in 2017-04-19 00:44:31

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -401,9 +401,7 @@ class QueryGenerator
                 foreach ($services as $service) {
                     $validServices[] = $service->service_id;
                 }
-                var_dump($validServices);
-                exit;
-                if ($validServices == $sentServices) {
+                if ($validServices == $sentServices || count($validServices) == 0 ) {
                     $newDocument = json_decode($oldDocument, false);
                     $oldToken = $newDocument->tokenId;
                     $newDocument->tokenId = $this->generateToken();
