c471d6ca5ff21fc9e9870bb90219cc9080bcb4a0
Nicolas Meinen <nick.meinen@live.com>
2017-04-22 17:15:22 -0300

    Devmind commit in 2017-04-22 17:15:58

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -63,7 +63,7 @@ class QueryGenerator
                 }
             }
             if ($uimsSyncTrigger == true){
-                echo "trigger o path";
+                //http_get(https://uims.uiot.com.br/api_devel/rest/devices/sync);
             }
             return $result;
         } elseif ($parsedPath->isValid() === false) {
