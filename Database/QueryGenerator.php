8c2968f174dcaf86f76e260a35103dafbaf191a9
Nicolas Meinen <nick.meinen@live.com>
2017-04-22 16:33:27 -0300

    Devmind commit in 2017-04-22 16:34:02

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -150,10 +150,6 @@ class QueryGenerator
                     } 
                 } elseif($chave == 'service_name'){
                     $queryStr = $this->appendToQuery($queryStr, $request->getParameters()[$key]); 
-                } elseif ($chave == 'start_date'){
-                   $queryStr = $queryStr.' server_time >'.$request->getParameters()['start_date'].' AND  ';
-                } elseif ($chave == 'end_date'){
-                   $queryStr = $queryStr.' server_time <'.$request->getParameters()['end_date'].' AND  ';
                 }    
             }
             $request->string = $this->finalizeQuery($request, $queryStr, false);
