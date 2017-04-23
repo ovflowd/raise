5af1e4551ede4a67c287c1e39bf37b9df5290826
Nicolas Meinen <nick.meinen@live.com>
2017-04-22 21:31:55 -0300

    Devmind commit in 2017-04-22 21:32:30

--- Database/QueryGenerator.php
+++ Database/QueryGenerator.php
@@ -75,11 +75,11 @@ class QueryGenerator
     }
     
     private function syncUimsService(){
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://uims.uiot.com.br/api_devel/rest/devices/sync");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);   
-        curl_exec($ch);
-        curl_close($ch);
+        //$ch = curl_init();
+        //curl_setopt($ch, CURLOPT_URL, "https://uims.uiot.com.br/api_devel/rest/devices/sync");
+        //curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);   
+        //curl_exec($ch);
+        //curl_close($ch);
     }
      
      /*
