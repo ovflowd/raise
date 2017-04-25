9d416ad1f0699afd81807c9287679e0317e39358
Iury Montenegro <iury2807@hotmail.com>
2017-04-25 17:14:11 -0300

    Devmind commit in 2017-04-25 17:14:47

--- Tests/index.php
+++ Tests/index.php
@@ -8,9 +8,14 @@ $tester = new RequestTester();
 //$tester->testInsertClient(); 
 //$tester->testInsertClientwithoutChannel();
 //$tester->testListAllClients();
-$token = $tester->testAutoRegister(); 
-echo '/n'."Testando POST Client Invalido".'/n';
-$tester->testInsertClient(false); 
-echo '/n'."Testando POST Service Invalido".'/n';
-$tester->registerServices($token, false);   
+
+for ($i = 0; $i < 1000; ++$i) {
+    $token = $tester->testAutoRegister(); 
+    echo '/n'."Testando POST Client Invalido".'/n';
+    $tester->testInsertClient(false); 
+    echo '/n'."Testando POST Service Invalido".'/n';
+    $tester->registerServices($token, false);
+    echo $i;
+}
+
 //$tester->registerServices("80ad313c43dbfe095cc5d76c4029f499");
