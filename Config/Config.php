88989c145a6cd833842748a80fb59993460135c1
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-05-11 19:34:12 -0300

    Update and rename Config.php to .gitkeep

--- Config/Config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-const DB_TYPE = 'COUCHBASE';
-const DB_ADDRESS = '127.0.0.1,user,pass'; //enter yours user and password conchbase
-const DB_IP = '127.0.0.1';
-const DB_USER = 'user';
-const DB_PASSWORD = 'password';
