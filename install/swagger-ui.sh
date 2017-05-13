730f5941cf5a3d7052df0914433371325854b367
Claudio Santoro <claudio.santoro@ieee.org>
2017-05-13 15:37:50 -0300

    Updated Swagger, Created Swagger API Routine

---
+++ install/swagger-ui.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SWAGGER_REPLACE="http://petstore.swagger.io/v2/swagger.json"
+SWAGGER_UIOT="https://raw.githubusercontent.com/sant0ro/RAISe/sbr/docs/swagger.json"
+
+cp -r vendor/swagger-api/swagger-ui/dist/* docs/swagger
+sed -i -e "s#$SWAGGER_REPLACE#$SWAGGER_UIOT#g" docs/swagger/index.html
