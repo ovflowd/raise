36a4230e8224c77f5f52f43ef0b8517ffe9888c1
Claudio Santoro <claudio.santoro@ieee.org>
2017-05-13 15:40:18 -0300

    Updated Swagger Script

--- install/swagger-ui.sh
+++ install/swagger-ui.sh
@@ -5,3 +5,4 @@ SWAGGER_UIOT="https://raw.githubusercontent.com/sant0ro/RAISe/sbr/docs/swagger.j
 
 cp -r vendor/swagger-api/swagger-ui/dist/* docs/swagger
 sed -i -e "s#$SWAGGER_REPLACE#$SWAGGER_UIOT#g" docs/swagger/index.html
+rm docs/swagger/index.html-e
