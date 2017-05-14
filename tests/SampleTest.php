39dcff140829a54a6f38b1f0776f72f3f1ba0045
Claudio Santoro <claudio.santoro@ieee.org>
2017-05-13 21:22:20 -0300

    Added Sample

---
+++ tests/SampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class SampleTest
+ */
+class SampleTest extends TestCase
+{
+    /**
+     * Sample Test of Functionality of PHPUnit
+     * @test
+     */
+    public function testBoolean()
+    {
+        $this->assertEquals(true, true);
+    }
+}
