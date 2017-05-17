4634a82d07463994ab5202fe98537fc6fe3567d5
Iury Montenegro Farias <iury2807@hotmail.com>
2017-05-17 17:33:20 -0300

    Couchbase Settings

--- app/Models/Settings/CouchbaseSettings.php
+++ app/Models/Settings/CouchbaseSettings.php
@@ -7,4 +7,24 @@ namespace App\Models\Settings;
  */
 class CouchbaseSettings
 {
+    /**
+     * ip address from the couchbase
+     *
+     * @var string
+     */
+    public $address = 'localhost';
+
+    /**
+     * user name from the couchbase
+     *
+     * @var string
+     */
+    public $user = 'couchbase';
+
+    /**
+     * password from the couchbase
+     *
+     * @var string
+     */
+    public $password = 'couchbase';
 }
