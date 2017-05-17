bb36cc0db0f0df3419e4d97695fd932eead48de5
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-05-17 19:22:51 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Models/Interfaces/Database.php
+++ app/Models/Interfaces/Database.php
@@ -3,9 +3,8 @@
 namespace App\Models\Interfaces;
 
 /**
- * Interface Database
+ * Interface Database.
  */
 interface Database
 {
-
 }
--- app/Models/Interfaces/Response.php
+++ app/Models/Interfaces/Response.php
@@ -3,9 +3,8 @@
 namespace App\Models\Interfaces;
 
 /**
- * Interface Response
+ * Interface Response.
  */
 interface Response
 {
-
 }
