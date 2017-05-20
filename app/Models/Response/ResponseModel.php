a30f5d249dec534ea2a451a137ed730d8ba372d3
Iury M. Farias <iury2807@hotmail.com>
2017-05-20 11:27:45 -0300

    Response Model

--- app/Models/Response/ResponseModel.php
+++ app/Models/Response/ResponseModel.php
@@ -7,4 +7,33 @@ namespace App\Models\Response;
  */
 class ResponseModel
 {
+    /**
+     * Reference HTTP code about response of request.
+     *
+     * @var String
+     */
+    public $codehttp;
+
+    /**
+     * Exception code from Couchbase Database functions.
+     *
+     * @var String
+     */
+    public $codecouchbase;
+
+    /**
+     * Message about HTTP code and/or Couchbase exception code.
+     *
+     * @var String
+     */
+    public $message;
+
+    /**
+     * Describes the message and displays the response body.
+     *
+     * @var String
+     */
+    public $decription;
+
+
 }
