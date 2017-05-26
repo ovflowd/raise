7a67d30926e671f6c1112185ea014d86be9d8d93
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-05-26 17:10:52 -0300

    Updated Test System

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -13,7 +13,7 @@ class ClientTest extends TestCase
      *
      * @var string
      */
-    public $serverUri = 'http://127.0.0.1/';
+    public $serverUri = 'http://127.0.0.1:8000/';
 
     /**
      * Test the Client Register.
@@ -23,15 +23,17 @@ class ClientTest extends TestCase
     public function testRegister()
     {
         $clientModel = [
-            'name'       => 'Sample Test',
-            'chipset'    => '0.0',
-            'mac'        => 'FF:FF:FF:FF:FF',
-            'serial'     => 'm3t41xR3l02d3d',
-            'processor'  => 'AMD SUX-K2',
-            'channel'    => 'ieee-4chan(nel)-802154',
+            'name' => 'Sample Test',
+            'chipset' => '0.0',
+            'mac' => 'FF:FF:FF:FF:FF',
+            'serial' => 'm3t41xR3l02d3d',
+            'processor' => 'AMD SUX-K2',
+            'channel' => 'ieee-4chan(nel)-802154',
             'clientTime' => microtime(true),
         ];
 
-        $response = Request::post($this->serverUri.'client/register')->sendsJson()->body($clientModel)->send();
+        $response = Request::post($this->serverUri . 'client/register')->sendsJson()->body($clientModel)->send();
+
+        $this->assertNotNull($response);
     }
 }
