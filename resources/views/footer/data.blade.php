40d2336578040ad85b6e5242debdac08e0b53a53
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-07-31 02:20:03 -0300

    Updated Data List Style

---
+++ resources/views/footer/data.blade.php
@@ -0,0 +1,22 @@
+<script src="@path/assets/js/vendor/jquery.js"></script>
+<script>
+    jQuery(document).ready(function () {
+        var table = jQuery('.callout.table h4');
+
+        table.append('<svg class="toggle" width="20" height="20"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#large-arrow"><svg viewBox="0 0 20 20" id="large-arrow" width="100%" height="100%"><path d="M13.25 10L6.109 2.58c-.268-.27-.268-.707 0-.979.268-.27.701-.27.969 0l7.83 7.908c.268.271.268.709 0 .979l-7.83 7.908c-.268.271-.701.27-.969 0-.268-.269-.268-.707 0-.979L13.25 10z"></path></svg></use></svg>');
+
+        table.parents('div').addClass('is-open');
+
+        jQuery(document).on('click', '.callout.table h4 svg.toggle', function () {
+            if(jQuery(this).parent().parent().hasClass('is-open')) {
+                jQuery(this).parent().parent().removeClass('is-open');
+
+                jQuery(this).parent().siblings().slideUp();
+            } else {
+                jQuery(this).parent().parent().addClass('is-open');
+
+                jQuery(this).parent().siblings().slideDown();
+            }
+        })
+    });
+</script>
