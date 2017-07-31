84307870485d8a254906ead36f956e356c692398
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-07-31 05:13:01 -0300

    Added Favicon (old one)

--- resources/views/header/welcome.blade.php
+++ resources/views/header/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>raise - welcome</title>
+    <link rel="shortcut icon" type="image/x-icon" href="@path/public/favicon.ico" />
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Source+Code+Pro:300,600|Titillium+Web:400,600,700" rel="stylesheet">
     <link rel="stylesheet" href="@path/assets/css/foundation.min.css">
     <link rel="stylesheet" href="@path/assets/css/app.min.css">
