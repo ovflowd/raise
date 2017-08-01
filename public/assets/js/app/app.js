77bdab639a73f0ce8414f2c9cdd4d92fbfe4cebf
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-07-31 23:35:08 -0300

    Updated Base Code

--- public/assets/js/app/app.js
+++ public/assets/js/app/app.js
@@ -1,11 +1,25 @@
 jQuery(document).foundation();
 
+function resizeControl() {
+    jQuery('#js-heightControl').css('height', jQuery(window).height() - jQuery('html').height() +'px');
+}
+
+jQuery(document).ready(function () {
+    resizeControl();
+});
+
+jQuery(window).resize(function () {
+    resizeControl();
+});
+
 jQuery(document).ready(function () {
     var table = jQuery('.callout.table h4');
 
     table.append('<svg class="toggle" width="20" height="20"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#large-arrow"><svg viewBox="0 0 20 20" id="large-arrow" width="100%" height="100%"><path d="M13.25 10L6.109 2.58c-.268-.27-.268-.707 0-.979.268-.27.701-.27.969 0l7.83 7.908c.268.271.268.709 0 .979l-7.83 7.908c-.268.271-.701.27-.969 0-.268-.269-.268-.707 0-.979L13.25 10z"></path></svg></use></svg>');
 
-    table.parent().addClass('is-open');
+    setTimeout(function () {
+        table.parent().addClass('is-open');
+    }, 1000);
 
     jQuery(document).on('click', '.callout.table h4 svg.toggle', function () {
         if(jQuery(this).parent().parent().hasClass('is-open')) {
@@ -17,7 +31,7 @@ jQuery(document).ready(function () {
 
             jQuery(this).parent().siblings().slideDown();
         }
-    })
+    });
 });
 
 jQuery(document).ready(function () {
@@ -55,15 +69,3 @@ function exploreElement(type, element) {
 
     return '<li><div class="callout primary"><a href="' + url + '" class="see-button">Watch</a><h5>' + element.document.name + ' <small>(' + type + ')</small></h5><small>[ ' + ((element.document.tags && element.document.tags.length) ? element.document.tags.join(', ') : 'No Tags') + ' ]</small></div></li>';
 }
-
-function resizeControl() {
-    jQuery('#js-heightControl').css('height', $(window).height() - $('html').height() +'px');
-}
-
-jQuery(document).ready(function () {
-    resizeControl();
-});
-
-jQuery(document).resize(function () {
-    resizeControl();
-});
