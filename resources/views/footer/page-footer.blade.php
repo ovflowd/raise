1ddee12acdc721cbc00f994d2e7a6b382201c249
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-08-01 01:31:12 -0300

    OK

--- resources/views/footer/page-footer.blade.php
+++ resources/views/footer/page-footer.blade.php
@@ -4,7 +4,7 @@
         <div class="footer">
             <small><b>© Copyright 2017 - <a href="http://www.unb.br/">University of Brasília</a></b></small><br>
             <small>this system uses swagger and zurb' foundation as base style.</small>
-            <img src="http://localhost/assets/svg/symbol-two.svg" style="">
+            <img src="assets/svg/symbol-two.svg" style="">
         </div>
     </div>
 </div>
