58f8fe09d08fffe00a404c3925eb13bc36f7b79d
Lucas Martins Alves <contato@lumal21.net.br>
2017-08-21 13:18:22 -0300

    Update client.blade.php

--- resources/views/body/client.blade.php
+++ resources/views/body/client.blade.php
@@ -1,4 +1,4 @@
-@<div class="grid-container">
+<div class="grid-container">
     <div class="grid-x grid-padding-x">
         <div class="large-12 medium-12 small-12 cell">
             @if($client->token->expireTime < time())
