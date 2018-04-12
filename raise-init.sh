d0889635c63a0787397b9f8b737fece9d1761c22
Claudio Santoro <claudio.santoro@redes.unb.br>
2018-04-12 20:39:23 -0300

    Raise Init

--- raise-init.sh
+++ raise-init.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-while [ wget -qO- couchbase:8091 &> /dev/null -ne 0 ]
-do
-    echo "Waiting Couchbase Start..."
-    sleep 5
-done
+wait_for_start() {
+    "$@"
+    while [ $? -ne 0 ]
+    do
+        echo 'Waiting for Couchbase to start'
+        sleep 3
+        "$@"
+    done
+}
 
 while [ ! -f /app/vendor/autoload.php ]
 do
-    echo "Waiting Composer..."
-    sleep 5
+  sleep 5
 done
+
+wait_for_start wget -qO- couchbase:8091 &> /dev/null
 
 php /app/install/install.php
 
